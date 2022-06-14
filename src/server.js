9b47f7e04e533b695a08e7148f19be7d3b06512f
seoljiwon <suljiwon@naver.com>
2022-06-15 00:09:55 +0900

    feat: #introduction

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+const __dirname = path.resolve();
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
